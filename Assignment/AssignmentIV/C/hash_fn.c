e2fc1599296d6990592042c97a7219f857854ab6
Julia Xiao <your_email@example.com>
2025-11-25 01:46:51 +0800

    Initial commit: add templates and skeleton.

---
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -0,0 +1,26 @@
+/*
+   ========================================
+   hash_fn.c — implement your hash functions
+   ========================================
+
+   Description:
+    This file contains the hash functions for integer and string keys.
+
+   Development History:
+    - 2025/11/22: Initial implementation
+
+   Developer: Miao-Xuan Xiao <s1113353@mail.yzu.edu.tw>
+ */
+
+#include "hash_fn.h"
+
+int myHashInt(int key, int m) {
+    // TODO: replace with your own design
+    return key % m;  // division method example
+}
+
+int myHashString(const char* str, int m) {
+    unsigned long hash = 0;
+    // TODO: replace with your own design
+    return (int)(hash % m); // basic division method
+}
---
+++ pu.bat
@@ -0,0 +1,7 @@
+git config user.name "Julia Xiao"
+git config user.email "your_email@example.com"
+
+git add .
+git commit -m "Initial commit: add templates and skeleton."     
+
+git push
