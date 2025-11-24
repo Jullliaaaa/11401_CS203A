c83593ccd38a398d0865888c632fe60e708a1b8c
Julia Xiao <your_email@example.com>
2025-11-25 03:17:49 +0800

    Implement basic string hash functions

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -8,19 +8,27 @@
 
    Development History:
     - 2025/11/22: Initial implementation
+    - 2025/11/24: Added basic hash function
 
    Developer: Miao-Xuan Xiao <s1113353@mail.yzu.edu.tw>
  */
 
 #include "hash_fn.h"
 
-int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+// Basic integer  hash function using simple modulo (division) method.
+int myHashInt(int key, int m) 
+{
     return key % m;  // division method example
 }
 
-int myHashString(const char* str, int m) {
+
+// Basic string hash function using simple ASCII sum.
+int myHashString(const char* str, int m) 
+{
     unsigned long hash = 0;
-    // TODO: replace with your own design
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+        hash += (unsigned char)str[i];   // add ASCII value of each char 
+    }
     return (int)(hash % m); // basic division method
 }
--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -8,18 +8,26 @@
 
    Development History:
     - 2025/11/22: Initial implementation
+    - 2025/11/24: Added basic hash function
 
    Developer: Miao-Xuan Xiao <s1113353@mail.yzu.edu.tw>
  */
 #include "hash_fn.hpp"
 
-int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+
+// Basic integer hash function using simple modulo (division) method.
+int myHashInt(int key, int m) 
+{
     return key % m;  // basic division method
 }
 
-int myHashString(const std::string& str, int m) {
+// Basic string hash function using simple ASCII sum.
+int myHashString(const std::string& str, int m) 
+{
     unsigned long hash = 0;
-    // TODO: replace with your own design
+    for (char c : str) 
+    {
+        hash += static_cast<unsigned char>(c);  // add ASCII value of each char
+    }
     return static_cast<int>(hash % m);  // basic division method
 }
